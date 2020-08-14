231e792d399203a28e4c781b8fb4098147835875
kyu9341 <kyu9341@naver.com>
2020-08-14 23:34:59 +0900

    게시글 요청 및 게시글 단위 자연어 처리

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const dotenv = require('dotenv')
-// .env 파일로 설정한 환경변수를 적용합니다(sample.env 파일 내용 확인 바람).
+// .env 파일로 설정한 환경변수를 적용합니다(.env 파일 내용 확인 바람).
 dotenv.config()
 
 const express = require('express')
 const path = require('path')
-
-const getData = require('./getData')
+const bodyParser = require('body-parser')
 const callTranslationApi = require('./callTranslationApi')
 const callNaturalLangApi = require('./callNaturalLangApi')
-const process_sentimentAnalysis = require('./process_sentimentAnalysis')
-const {convertFormatForAnalysis, convertFormatForUI} = require('./convertFormat')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extends: true }))
 app.set('port', process.env.PORT || 3000)
-
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.post('/posts', async (req, res) => {
+  const post = req.body;
+  const translatedPost = await callTranslationApi(post)
+  const processedPost = await callNaturalLangApi(translatedPost);
+  // TODO INSERT post, 추가적으로 ascii 텍스트 처리해서 넣어주어야 합니다.
+  console.log('processedPost', processedPost);
+
+  res.redirect('/');
+});
+
+/*
+ex)
+processedPost {
+  title: '하하하',
+  contents: '하하하하 안녕하세요~',
+  translatedText: 'Hahahaha Hello~',
+  sentiment: 'neutral' // 감정 분석 결과 neutral, positive, negative
+}
+*/
+
 app.get('/posts', (req, res) => {
-  getData() // json 파일 가져옴
-    .then(convertFormatForAnalysis)
-    .then(contents => new Promise((resolve, reject) => {
-      Promise.all(contents.map(callTranslationApi)).then(resolve)
-    }))
-    /**
-     * api 호출 함수가 두 종류(callNaturalLangApi, process_sentimentAnalysis)가 있는데
-     * 하나를 선택하여 사용하시면 됩니다.
-     */
-    .then(callNaturalLangApi)
-    // .then(process_sentimentAnalysis) //출력 // data => res.json(data)
-    .then(convertFormatForUI)
-    .then(posts => res.json(posts))
-    .catch(error => {
-      console.log(error)
-      let substitute = require('./public/sentiment.json')
-      if (!substitute) {
-        substitute = require('./public/data.json')
-      }
-      res.json(substitute)
-    })
+
+  // TODO DB에서 select 한 데이터를 출력해주는 코드
+  res.json();
 })
 
 app.listen(app.get('port'), () => {
